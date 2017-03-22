898ffe7872a72c8f850d1d5e350decbf4f27df9c
Cody Turnock <ummcody@gmail.com>
2017-03-21 23:22:19 -0400

    things

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,40 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 
-var sass = require('gulp-sass');
+var sass        = require('gulp-sass');
 
-var minifycss = require('gulp-minify-css');
+var minifycss   = require('gulp-minify-css');
+
+var plumber     = require('gulp-plumber');
+
+var notifier    = require('node-notifier');
+
+var notify      = require('gulp-notify');
+
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/style.scss')
-        .pipe(sass())
+        .pipe(plumber())
+        .pipe(sass()
+            .on("error", notify.onError({
+                title: "Sass Error",
+                message: "Error: <%= error.message %>"
+            }))
+        )
         .pipe(minifycss())
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['sass', 'browser-sync'], function() {
     gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
